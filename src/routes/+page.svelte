0399232bff92b0208dbd5a3af86e3694db94c3a9
davis9001 <monaghan.david@gmail.com>
2024-03-03 01:30:37 -0700

    hide overflow on reddit buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -99,7 +99,7 @@
         {#each subreddits["For Discord"] as subreddit}
           <a
             href="{subreddit}/submit?title={postTitle}&url={inviteLink}"
-            class="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            class="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded overflow-hidden"
             target="_blank"
           >
             {subreddit}
