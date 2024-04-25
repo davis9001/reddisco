bb879b2832ca0fac8ab9bde259349ba9cefc8257
davis9001 <monaghan.david@gmail.com>
2024-04-25 07:04:32 -0700

    Link to starspace.group

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <slot class="m-0 p-0" />
 
 <footer class="text-center bg-black text-white p-8">
-  <a href="https://discord.gg/SAjnQt5rdC" target="_blank" class="font-bold"
+  <a href="https://starspace.group" target="_blank" class="font-bold"
     >Brought to you by *Space</a
   >
   | <i>Made with SvelteKit and hosted on Cloudflare</i> |
