751ce109fe711fec21685841045fe869db2c6377
davis9001 <monaghan.david@gmail.com>
2024-10-15 06:28:50 -0700

    Upgrade gemini module and use 1.5-pro

--- src/routes/api/ai/+server.ts
+++ src/routes/api/ai/+server.ts
@@ -1,22 +1,22 @@
-import { error } from "@sveltejs/kit"
-import { GoogleGenerativeAI } from "@google/generative-ai"
-import { GOOGLEAI_API_KEY } from "$env/static/private"
+import { error } from "@sveltejs/kit";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GOOGLEAI_API_KEY } from "$env/static/private";
 
 /** @type {import('./$types').RequestHandler} */ export async function GET({
   url,
 }) {
-  const postTitle = url.searchParams.get("postTitle") ?? ""
-  const promptAdvice = url.searchParams.get("promptAdvice") ?? ""
+  const postTitle = url.searchParams.get("postTitle") ?? "";
+  const promptAdvice = url.searchParams.get("promptAdvice") ?? "";
   if (!postTitle) {
-    throw error(400, "postTitle is required")
+    throw error(400,"postTitle is required");
   }
 
   const generatedPostTitle = await generateRedditPostTitle(
     postTitle,
     promptAdvice
-  )
+  );
 
-  return new Response(JSON.stringify({ generatedPostTitle }))
+  return new Response(JSON.stringify({ generatedPostTitle }));
 }
 
 async function generateRedditPostTitle(
@@ -24,21 +24,21 @@ async function generateRedditPostTitle(
   promptAdvice: string
 ): Promise<string> {
   try {
-    const genAI = new GoogleGenerativeAI(GOOGLEAI_API_KEY)
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+    const genAI = new GoogleGenerativeAI(GOOGLEAI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-    const prompt = `As an advertisement for our Discord server, we want to create a catchy Reddit post title. The current title is: "${title}". ${
-      promptAdvice
-        ? `Use this as help to guide this prompt: "${promptAdvice}".`
-        : ""
-    } We need a version that captures attention and encourages engagement. Return just a string of text with no special characters other than maybe an emoji or two.`
+    const prompt = `As an advertisement for our Discord server, we want to create a catchy Reddit post title. The current title is: "${title}". ${promptAdvice
+      ? `Use this as help to guide this prompt: "${promptAdvice}".`
+      : ""
+      } We need a version that captures attention and encourages engagement. Return just a string of text with no special characters other than maybe an emoji or two.`;
 
-    const result = await model.generateContent(prompt)
-    const response = result.response
-    const text = response.text()
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
 
-    return text
+    return text;
   } catch (error: any) {
-    throw new Error(`Failed to generate Reddit post title: ${error.message}`)
+    return title;
+    // throw new Error(`Failed to generate Reddit post title: ${error.message}`);
   }
 }
